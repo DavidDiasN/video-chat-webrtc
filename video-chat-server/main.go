e34b72c3a078dff1ac6e8af3d6f3e6a496aedce7
DavidDiasN <daviddiasnogueira@gmail.com>
2024-05-19 23:22:16 -0400

    I need to read my error messages more carefully.

--- video-chat-server/main.go
+++ video-chat-server/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"github.com/google/uuid"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 	"io"
+	"log"
 	"net/http"
 	"os"
-
-	"github.com/google/uuid"
-	"github.com/gorilla/websocket"
+	"time"
 )
 
 var upgrader = websocket.Upgrader{
@@ -21,113 +25,36 @@ type ConnInfo struct {
 	uuid string
 }
 
+var postedMessages = map[string]ConnInfo{"david": ConnInfo{websocket.Conn{}, uuid.NewString()}, "Michael": ConnInfo{websocket.Conn{}, uuid.NewString()}, "George": ConnInfo{websocket.Conn{}, uuid.NewString()}}
+
+//postedMessages := map[string]websocket.Conn{"david": websocket.Conn{}, "Michael": websocket.Conn{}, "George": websocket.Conn{}}
+
 func main() {
 
-	//postedMessages := map[string]websocket.Conn{"david": websocket.Conn{}, "Michael": websocket.Conn{}, "George": websocket.Conn{}}
-	postedMessages := map[string]ConnInfo{"david": ConnInfo{websocket.Conn{}, uuid.NewString()}, "Michael": ConnInfo{websocket.Conn{}, uuid.NewString()}, "George": ConnInfo{websocket.Conn{}, uuid.NewString()}}
+	var dir string
 
-	makeofferFile, err := os.Open("assets/js/makeoffer.js")
-	if err != nil {
-		fmt.Println(err)
-		return
+	flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Default to the current directory")
+	flag.Parse()
+	router := mux.NewRouter()
+	router.PathPrefix("/static/assets/js").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
+	router.HandleFunc("/videocall/getoffers", videocallGetOffers)
+	router.HandleFunc("/videocall/MakeOffer", videocallMakeOffer)
+	router.HandleFunc("/videocall/offernameValidation", videocallOfferNameValidation).Methods("POST")
+	router.HandleFunc("/videocall/MakeOffer/ws", videocallMakeOfferWS)
+
+	srv := &http.Server{
+		Handler: handlers.CORS(
+			handlers.AllowedMethods([]string{"HEAD", "POST", "OPTIONS", "GET", "PUT"}),
+			handlers.AllowedOrigins([]string{"*"}),
+			handlers.AllowedHeaders([]string{"X-Requested-With", "Accept", "Content-Type", "Content-Length", "Accept-Encoding", "Authorization", "X-CSRF-Token"}),
+			//	handlers.AllowCredentials(),
+			//handlers.IgnoreOptions()
+		)(router),
+		Addr:         os.Getenv("WEB_SERVER_ADDRESS"),
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
-	makeofferInfo, err := makeofferFile.Stat()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	makeofferFileBytes := make([]byte, makeofferInfo.Size())
-
-	makeofferFile.Read(makeofferFileBytes)
-
-	getoffersFile, err := os.Open("assets/js/getoffers.js")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	getoffersInfo, err := getoffersFile.Stat()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	getoffersFileBytes := make([]byte, getoffersInfo.Size())
-
-	getoffersFile.Read(getoffersFileBytes)
-
-	// need to manage connections. I must take them, hold them, discard them, transmit them.
-
-	http.HandleFunc("/videocall/getoffers", func(w http.ResponseWriter, r *http.Request) {
-
-		if r.Method == "GET" {
-			keys := []string{}
-			for k := range postedMessages {
-				keys = append(keys, k)
-			}
-			getPage := getOffersPage(keys)
-			getPage.Render(context.Background(), w)
-
-		} else {
-			fmt.Println("not a get request")
-		}
-
-	})
-
-	http.HandleFunc("/videocall/makeoffer", func(w http.ResponseWriter, r *http.Request) {
-
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token")
-		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
-		comp := makeoffer()
-		comp.Render(context.Background(), w)
-
-	})
-
-	http.HandleFunc("/videocall/offernameValidation", func(w http.ResponseWriter, r *http.Request) {
-
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token")
-		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
-		responseBody, err := io.ReadAll(r.Body)
-		must(err)
-		cleanedInput := string(responseBody)
-		for k := range postedMessages {
-			if k == cleanedInput {
-				w.Write([]byte("NO"))
-				return
-			}
-		}
-		returnUUID := uuid.NewString()
-		postedMessages[cleanedInput] = ConnInfo{websocket.Conn{}, returnUUID}
-		w.Write([]byte(returnUUID))
-	})
-
-	http.HandleFunc("/videocall/makeoffer/ws", func(w http.ResponseWriter, r *http.Request) {
-		//check body of request for name and uuid
-		requestMessage, err := io.ReadAll(r.Body)
-		must(err)
-		fmt.Println(string(requestMessage))
-		if len(requestMessage) > 2 {
-			fmt.Println("has size 2 or more ")
-		}
-		//conn, err := upgrader.Upgrade(w, r, nil)
-		//must(err)
-		//conn.WriteMessage(1, []byte("Hello my friend"))
-
-	})
-
-	http.HandleFunc("/videocall/assets/js/makeoffer.js", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/javascript")
-		w.Write(makeofferFileBytes)
-	})
-
-	http.HandleFunc("/videocall/assets/js/getoffers.js", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/javascript")
-		w.Write(getoffersFileBytes)
-	})
-
-	fmt.Errorf("err %s", http.ListenAndServe(":4009", nil))
+	log.Fatal(srv.ListenAndServe())
 }
 
 func must(err error) {
@@ -136,4 +63,67 @@ func must(err error) {
 		fmt.Print("See ya")
 		panic(err)
 	}
+}
+
+// /videocall/getoffers"
+
+func videocallGetOffers(w http.ResponseWriter, r *http.Request) {
+	//	w.Header().Add("Access-Control-Allow-Origin", "*")
+	if r.Method == "GET" {
+		keys := []string{}
+		for k := range postedMessages {
+			keys = append(keys, k)
+		}
+		getPage := getOffersPage(keys)
+		getPage.Render(context.Background(), w)
+
+	} else {
+		fmt.Println("not a get request")
+	}
+
+}
+
+// "/videocall/makeoffer"
+
+func videocallMakeOffer(w http.ResponseWriter, r *http.Request) {
+
+	comp := makeoffer()
+	comp.Render(context.Background(), w)
+
+}
+
+//"/videocall/offernameValidation"
+
+func videocallOfferNameValidation(w http.ResponseWriter, r *http.Request) {
+
+	//	w.Header().Add("Access-Control-Allow-Origin", "*")
+
+	responseBody, err := io.ReadAll(r.Body)
+	must(err)
+	cleanedInput := string(responseBody)
+	for k := range postedMessages {
+		if k == cleanedInput {
+			w.Write([]byte("NO"))
+			return
+		}
+	}
+	returnUUID := uuid.NewString()
+	postedMessages[cleanedInput] = ConnInfo{websocket.Conn{}, returnUUID}
+	w.Write([]byte(returnUUID))
+}
+
+//"/videocall/makeoffer/ws",
+
+func videocallMakeOfferWS(w http.ResponseWriter, r *http.Request) {
+	//check body of request for name and uuid
+	requestMessage, err := io.ReadAll(r.Body)
+	must(err)
+	fmt.Println(string(requestMessage))
+	if len(requestMessage) > 2 {
+		fmt.Println("has size 2 or more ")
+	}
+	//conn, err := upgrader.Upgrade(w, r, nil)
+	//must(err)
+	//conn.WriteMessage(1, []byte("Hello my friend"))
+
 }
